b4d08fc8d54f73d68972e126cdfb3bac470f91cb
Eduarks <n00291389@upn.pe>
2024-07-08 02:56:19 -0500

    add component motive

--- src/app/components/mantenimiento/listar-motivo/listar-motivo.component.ts
+++ src/app/components/mantenimiento/listar-motivo/listar-motivo.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listar-motivo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listar-motivo.component.html',
   styleUrl: './listar-motivo.component.css'
 })
